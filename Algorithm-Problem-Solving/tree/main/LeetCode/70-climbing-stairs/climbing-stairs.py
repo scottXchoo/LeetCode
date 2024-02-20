358a035cc87d296faf69b6244eeb09f8c9702b6b
scottXchoo <107841492+scottXchoo@users.noreply.github.com>
2024-02-20 15:47:03 +0900

    Time: 28 ms (92.21%) | Memory: 16.6 MB (77.59%) - LeetSync

--- Algorithm-Problem-Solving/tree/main/LeetCode/70-climbing-stairs/climbing-stairs.py
+++ Algorithm-Problem-Solving/tree/main/LeetCode/70-climbing-stairs/climbing-stairs.py
@@ -1,13 +1,7 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        memo = {}
-        def fibo(n):
-            if n == 1:
-                return 1
-            if n == 2:
-                return 2
-            if n not in memo:
-                memo[n] = fibo(n-1) + fibo(n-2)
-            return memo[n]
-        
-        return fibo(n)
+        dp = [-1] * (n + 1)
+        dp[0], dp[1] = 1, 1
+        for i in range(2, n + 1):
+            dp[i] = dp[i - 1] + dp[i - 2]
+        return dp[n]
