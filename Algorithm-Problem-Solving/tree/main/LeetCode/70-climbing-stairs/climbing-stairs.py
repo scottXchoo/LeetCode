6461226c03f20c510050e0ec757a181a65a8b587
scottXchoo <107841492+scottXchoo@users.noreply.github.com>
2024-02-20 15:47:29 +0900

    Time: 36 ms (51.67%) | Memory: 16.5 MB (77.59%) - LeetSync

--- Algorithm-Problem-Solving/tree/main/LeetCode/70-climbing-stairs/climbing-stairs.py
+++ Algorithm-Problem-Solving/tree/main/LeetCode/70-climbing-stairs/climbing-stairs.py
@@ -1,7 +1,10 @@
 class Solution:
-    def climbStairs(self, n: int) -> int:
-        dp = [-1] * (n + 1)
-        dp[0], dp[1] = 1, 1
-        for i in range(2, n + 1):
-            dp[i] = dp[i - 1] + dp[i - 2]
-        return dp[n]
+        def climbStairs(self, n):
+            memo = [-1] * (n + 1)
+            def dp(n):
+                if n == 0 or n == 1:
+                    return 1
+                if memo[n] == -1:
+                    memo[n] = dp(n - 1) + dp(n - 2)
+                return memo[n]
+            return dp(n)
