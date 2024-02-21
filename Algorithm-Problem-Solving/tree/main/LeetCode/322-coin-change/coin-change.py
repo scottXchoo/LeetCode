5dab7e26204ec96bc487f9d4445c5ad0065c7663
scottXchoo <107841492+scottXchoo@users.noreply.github.com>
2024-02-21 10:33:55 +0900

    Time: 857 ms (44.39%) | Memory: 16.8 MB (94.91%) - LeetSync

--- Algorithm-Problem-Solving/tree/main/LeetCode/322-coin-change/coin-change.py
+++ Algorithm-Problem-Solving/tree/main/LeetCode/322-coin-change/coin-change.py
@@ -1,19 +1,11 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        memo = {}
+        dp = [sys.maxsize for _ in range(amount + 1)]
+        dp[0] = 0
 
-        def dp(amount):
-            if amount == 0:
-                return 0
-
-            retList = []
+        for i in range(amount + 1):
             for coin in coins:
-                remain_amount = amount - coin
-                if remain_amount >= 0:
-                    if remain_amount not in memo:
-                        memo[remain_amount] = dp(remain_amount)
-                    if memo[remain_amount] != -1:
-                        retList.append(memo[remain_amount])
-            return min(retList) + 1 if retList else -1
+                if i + coin <= amount:
+                    dp[i+coin] = min(dp[i+coin], dp[i]+1)
 
-        return dp(amount)
+        return -1 if dp[amount] == sys.maxsize else dp[amount]
